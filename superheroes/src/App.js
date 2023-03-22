1bb50837fc654098134a2c03c0dd93f66c748f59
Natalia <alt.antares@gmail.com>
2023-03-22 12:08:27 +0300

    added heroes in array

--- superheroes/src/App.js
+++ superheroes/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Hero from './components/hero';
+import heroes from './utils/arrHeroes';
 
 function App() {
   return (
     <div className="App">
-      <Hero name="Железный человек" universe="Marvel" alterego="Тони Старк" activity="Гений" superpower="Высокий уровень интеллекта" img="ironman.png" alt="man in red iron suit"></Hero>
-      <Hero name="Халк" universe="Marvel" alterego="Брюс Беннер" activity="Борец за справедливость" superpower="Cверхчеловеческая сила" img="halk.jpg" alt="big green strong man"></Hero>
-      <Hero name="Черная вдова" universe="Marvel" alterego="Наташа Романофф" activity="Шпионка" superpower="Пик человеческого физического потенциала" img="blackwidow.jpg" alt="a woman in a black suit with a weapon on the background of a night city"></Hero>
+      {
+        heroes.map((hero) =>
+          <Hero name={hero.name} universe={hero.universe} alterego={hero.alterego} activity={hero.activity} superpower={hero.superpower} img={hero.img} alt={hero.alt}></Hero>
+        )
+      }
     </div>
   );
 }
---
+++ superheroes/src/utils/arrHeroes.js
@@ -0,0 +1,10 @@
+const heroes = [
+    { name: "Железный человек", universe: "Marvel", alterego: "Тони Старк", activity: "Гений", superpower: "Высокий уровень интеллекта", img: "ironman.png", alt: "man in red iron suit" },
+
+    { name: "Черная вдова", universe: "Marvel", alterego: "Наташа Романофф", activity: "Шпионка", superpower: "Пик человеческого физического потенциала", img: "blackwidow.jpg", alt: "a woman in a black suit with a weapon on the background of a night city" },
+
+    { name: "Халк", universe: "Marvel", alterego: "Брюс Беннер", activity: "Борец за справедливость", superpower: "Cверхчеловеческая сила", img: "halk.jpg", alt: "big green strong man" }
+
+];
+
+export default heroes;
